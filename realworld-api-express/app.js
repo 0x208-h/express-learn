b420fc767b9d0dd9864436a0a3485d8ab0e6ab10
0x208-h <huangchenhui0812@163.com>
2022-02-08 19:33:04 +0800

    常见中间件

--- realworld-api-express/app.js
+++ realworld-api-express/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
