bfdb19ea7a32370594bb9e11dc128e24b88d4ec5
0x208-h <huangchenhui0812@163.com>
2022-02-09 00:34:13 +0800

    统一处理错误 中间件

--- realworld-api-express/app.js
+++ realworld-api-express/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const router = require("./router")
-
+const errorHandler = require("./middleware/errorHandler")
 const app = express();
 
 app.use(morgan("dev"));
@@ -10,6 +10,9 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api', router)
+
+//统一处理错误中间件
+app.use(errorHandler())
 
 const PORT = process.env.PORT || 3000;
 
