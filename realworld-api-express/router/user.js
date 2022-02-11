d4de86ec0b62727cd752195ecb75cc87cecd3ba5
0x208-h <huangchenhui0812@163.com>
2022-02-11 21:24:24 +0800

    验证用户名是否存在

--- realworld-api-express/router/user.js
+++ realworld-api-express/router/user.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const { body, validationResult } = require("express-validator");
 const userCtrl = require("../controller/user");
+const { db } = require("../util/db");
 const router = express.Router();
 // 用户登录
 router.post("/users/login", userCtrl.login);
 // 用户注册
 router.post(
   "/users",
-  [body("username").notEmpty().withMessage("用户名不能为空")],
+  [
+    body("username")
+      .notEmpty()
+      .withMessage("用户名不能为空")
+      .custom(async (value) => {
+        console.log(value, "value");
+        const user = await db("select * from users where username = ?", [
+          value
+        ]);
+        if (user) {
+          return Promise.reject("用户名已经存在 ");
+        }
+      }),
+  ],
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
